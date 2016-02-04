d9813ba775649d3dc5bb2b2c3ffc96f28136846c
Jeremy Summers <jez@wizard.technology>
2016-02-04 12:40:39 +0000

    configured to use vbguest and vagrant cachier, swapped box (Chef/Guest Adds/Fedora 18)

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# VERSION="1.5.3"
+# OS="linux"
+# ARCH="amd64"
+#yum install go -y
+# wget https://storage.googleapis.com/golang/go$VERSION.$OS-$ARCH.tar.gz
+# tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
+# export PATH=$PATH:/usr/local/go/bin
